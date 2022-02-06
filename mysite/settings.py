642b07afdc19d4b3cbecff6347108271653cf800
alvi-2081 <abdullah.alvi22@gmail.com>
2022-02-06 14:19:58 +0500

    Added

--- mysite/settings.py
+++ mysite/settings.py
@@ -54,8 +54,10 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 ROOT_URLCONF = 'mysite.urls'
+
 
 TEMPLATES = [
     {
